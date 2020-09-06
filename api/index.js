d4fd5900b731b2bd501efb0abe5e98c26dd4533a
JamesPatrickGill <jamespatrickgill2@gmail.com>
2020-09-06 23:11:23 +0100

    feat: working commit

--- api/index.js
+++ api/index.js
@@ -1,3 +1,20 @@
+import {generate} from "../lib/generate";
+
+const extractUrlParams = (req) => {
+    return req.url.split('?')[1].split("&").map((el) => {
+        return {[el.split('=')[0]]: el.split('=')[1]}
+    }).reduce((params, el) => {
+        return {...params, ...el}
+    }, {})
+}
+
+
 export default (req, res) => {
-    res.json({name: 'John', email: 'john@example.com'})
+    const options = JSON.parse(decodeURIComponent(extractUrlParams(req)['options']))
+    console.log(options)
+    if (!options) {
+        res.send({body: "No arguments provided"})
+    }
+    res.send(`${generate(options)}`);
+
 }
